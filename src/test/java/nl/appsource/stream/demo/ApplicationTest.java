3fea0668b0b4106a968ad3c0b72f99c715770f5a
Bas Huisman <bhuism@odee.net>
2019-10-15 19:49:41 +0200

    clean tests

--- src/test/java/nl/appsource/stream/demo/ApplicationTest.java
+++ src/test/java/nl/appsource/stream/demo/ApplicationTest.java
@@ -1,7 +1,6 @@
 package nl.appsource.stream.demo;
 
 import nl.appsource.stream.demo.model.Citaat;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,26 +14,25 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.collection.IsMapContaining.hasEntry;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -47,58 +45,73 @@ public class ApplicationTest {
         return "http://localhost:" + port;
     }
 
-    private ResponseEntity<Citaat> getCitaat(final Long id) {
-        final String URI = baseUrl() + "/citaten/{id}";
-        final RestTemplate restTemplate = new RestTemplate();
-
-        restTemplate.setMessageConverters(getMessageConverters());
-
+    private HttpEntity<Citaat> createHttpEntity(final Citaat citaat) {
         final HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaTypes.HAL_JSON));
-        final HttpEntity<String> entity = new HttpEntity<>(headers);
+        final HttpEntity<Citaat> entity = new HttpEntity<>(citaat, headers);
+        return entity;
+    }
 
-        return restTemplate.exchange(URI, HttpMethod.GET, entity, Citaat.class, id);
+    private ResponseEntity<Citaat> getCitaat(final Long id) {
+        final String url = baseUrl() + "/citaten/{id}";
+        final RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.exchange(url, HttpMethod.GET, createHttpEntity(null), Citaat.class, id);
     }
 
     private CollectionModel<EntityModel<Citaat>> getCitaten() throws URISyntaxException {
 
         Traverson traverson = new Traverson(new URI(baseUrl() + "/citaten"), MediaTypes.HAL_JSON);
         Traverson.TraversalBuilder tb = traverson.follow("citaten");
-        ParameterizedTypeReference<CollectionModel<EntityModel<Citaat>>> typeRefDevices = new ParameterizedTypeReference<>() {};
+        ParameterizedTypeReference<CollectionModel<EntityModel<Citaat>>> typeRefDevices = new ParameterizedTypeReference<>() {
+        };
         final CollectionModel<EntityModel<Citaat>> models = tb.toObject(typeRefDevices);
 
         return models;
     }
 
+    private ResponseEntity<Citaat> createCitaat(final Citaat citaat) throws URISyntaxException {
+        final String url = baseUrl() + "/citaten";
+        final RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForEntity(url, createHttpEntity(citaat), Citaat.class);
+    }
+
     @Test
-    public void testGetCitatenSuccess() {
+    public void testGetCitaat() {
 
         final ResponseEntity<Citaat> response = getCitaat(1102L);
         final Citaat resource = response.getBody();
 
-        assertThat(response.getStatusCodeValue(), is(200));
+        assertThat(response.getStatusCode(), is(equalTo(OK)));
         assertThat(response.getHeaders().keySet(), hasItem("Content-Type"));
         assertThat(response.getHeaders(), hasEntry("Content-Type", Collections.singletonList(MediaTypes.HAL_JSON_VALUE)));
 
         assertThat(resource, is(not(nullValue())));
-        assertThat(resource.getId(), is(1102L));
+        assertThat(resource.getId(), is(equalTo(1102L)));
     }
 
     @Test
-    public void testGetCitaatSuccess() throws URISyntaxException {
+    public void testGetCitaten() throws URISyntaxException {
         final CollectionModel<EntityModel<Citaat>> response = getCitaten();
         final Collection<EntityModel<Citaat>> resources = response.getContent();
         assertThat(resources.size(), is(greaterThan(0)));
-        assertThat(resources,  hasSize(5));
+        assertThat(resources, hasSize(5));
     }
 
-    private List<HttpMessageConverter<?>> getMessageConverters() {
-        final List<HttpMessageConverter<?>> converters = new ArrayList<>();
-        converters.add(new MappingJackson2HttpMessageConverter());
-        return converters;
+    @Test
+    public void testCreateCitaat() throws URISyntaxException {
+        final Citaat citaat = new Citaat(null, "HiThere", 38291L, 73001L);
+
+        final ResponseEntity<Citaat> response = createCitaat(citaat);
+        final Citaat resource = response.getBody();
+
+        assertThat(response.getStatusCode(), is(equalTo(CREATED)));
+        assertThat(response.getHeaders().keySet(), hasItem("Content-Type"));
+        assertThat(response.getHeaders(), hasEntry("Content-Type", Collections.singletonList(MediaTypes.HAL_JSON_VALUE)));
+
+        assertThat(resource, is(not(nullValue())));
+        assertThat(resource.getName(), is(equalTo("HiThere")));
 
 
     }
-
 
 }
