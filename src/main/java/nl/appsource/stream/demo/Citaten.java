cc3ce4d8f517a9e527c5606bdcdd7264317dde01
Bas Huisman <bhuism@odee.net>
2019-10-15 17:02:49 +0200

    exclude sprintbootapplication class

--- src/main/java/nl/appsource/stream/demo/Citaten.java
+++ src/main/java/nl/appsource/stream/demo/Citaten.java
@@ -1,5 +1,6 @@
 package nl.appsource.stream.demo;
 
+import lombok.Generated;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import static org.springframework.hateoas.config.EnableHypermediaSupport.Hyperme
 @Slf4j
 @SpringBootApplication
 @EnableHypermediaSupport(type = HAL)
+@Generated
 public class Citaten {
 
     public static void main(String[] args) {
