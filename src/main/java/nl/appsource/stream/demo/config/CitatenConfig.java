4cd2c9ecfdcdf742a443337c1e83d1150a8c5458
Bas Huisman <bhuism@odee.net>
2019-10-17 23:03:39 +0200

    disable csrf

--- src/main/java/nl/appsource/stream/demo/config/CitatenConfig.java
+++ src/main/java/nl/appsource/stream/demo/config/CitatenConfig.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,21 +21,20 @@ public class CitatenConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
     }
 
     @Bean
-    @Profile("production")
     public CommonsRequestLoggingFilter logFilter() {
         final CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
         filter.setIncludeQueryString(true);
         filter.setIncludePayload(true);
         filter.setMaxPayloadLength(10000);
-        filter.setIncludeHeaders(false);
+        filter.setIncludeHeaders(true);
         return filter;
     }
 
     @Bean
-    @Profile("production")
     public FilterRegistrationBean<Filter> forwardedHeaderFilter() {
         final FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>();
         bean.setFilter(new ForwardedHeaderFilter());
