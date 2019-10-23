d705fa0500a5cb3c3134ce2946d42ab74755ed59
Bas Huisman <bhuism@odee.net>
2019-10-23 16:49:38 +0200

    fix tests

--- src/test/java/nl/appsource/stream/demo/ApplicationTest.java
+++ src/test/java/nl/appsource/stream/demo/ApplicationTest.java
@@ -2,15 +2,13 @@ package nl.appsource.stream.demo;
 
 import lombok.extern.slf4j.Slf4j;
 import nl.appsource.stream.demo.model.Citaat;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriTemplate;
 
@@ -35,7 +33,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTest {
 
