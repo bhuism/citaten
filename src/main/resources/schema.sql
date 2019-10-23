a23e89f8d9a8bddc79cebbd0ec072f34bf4963c7
Bas Huisman <bhuism@odee.net>
2019-10-23 22:58:30 +0200

    better sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,24 +1,30 @@
 CREATE TABLE `Spreker`
 (
-    `id`   bigint(20)   NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
+    `id`   bigint(20)                        NOT NULL AUTO_INCREMENT,
+    `name` varchar(255)                      NOT NULL,
+    `uuid` VARCHAR(36) DEFAULT RANDOM_UUID() NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE (`name`)
 );
 
 CREATE TABLE `Categorie`
 (
-    `id`   bigint(20) NOT NULL AUTO_INCREMENT,
+    `id`   bigint(20)                         NOT NULL AUTO_INCREMENT,
     `name` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    `uuid` VARCHAR(36)  DEFAULT RANDOM_UUID() NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE (`name`)
 );
 
 CREATE TABLE `Citaat`
 (
-    `id`        bigint(20)    NOT NULL AUTO_INCREMENT,
-    `name`      varchar(4096) NOT NULL,
-    `categorie` bigint(20)    NOT NULL,
-    `spreker`   bigint(20)    NOT NULL,
+    `id`        bigint(20)                        NOT NULL AUTO_INCREMENT,
+    `name`      varchar(4096)                     NOT NULL,
+    `categorie` bigint(20)                        NOT NULL,
+    `spreker`   bigint(20)                        NOT NULL,
+    `uuid`      VARCHAR(36) DEFAULT RANDOM_UUID() NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE (`name`),
     CONSTRAINT `FK785288E6469F3E0A` FOREIGN KEY (`categorie`) REFERENCES `Categorie` (`id`),
     CONSTRAINT `FK785288E651A78AC` FOREIGN KEY (`spreker`) REFERENCES `Spreker` (`id`)
 );
