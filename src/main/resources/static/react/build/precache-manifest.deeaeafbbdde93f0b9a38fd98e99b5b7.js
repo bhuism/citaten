5121b619dcb3d7ecdf2f79945037b2bce5fd894a
Bas Huisman <bhuism@odee.net>
2019-11-21 16:44:13 +0100

    add react app

---
+++ src/main/resources/static/react/build/precache-manifest.deeaeafbbdde93f0b9a38fd98e99b5b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4be92ccee7fc2c73db058d4d2d2a23c",
+    "url": "/react/build/index.html"
+  },
+  {
+    "revision": "072f0ff1327313e50240",
+    "url": "/react/build/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9a44a182c8455e87a124",
+    "url": "/react/build/static/js/2.c95037dc.chunk.js"
+  },
+  {
+    "revision": "072f0ff1327313e50240",
+    "url": "/react/build/static/js/main.01b85819.chunk.js"
+  },
+  {
+    "revision": "5e1835466a557a07b543",
+    "url": "/react/build/static/js/runtime-main.a9303b15.js"
+  }
+]);
