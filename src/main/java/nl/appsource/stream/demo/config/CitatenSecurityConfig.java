eec066c5354976822c9d6302b4a9d94b079ad604
Bas Huisman <bhuism@odee.net>
2019-11-25 13:52:27 +0100

    add cors

--- src/main/java/nl/appsource/stream/demo/config/CitatenSecurityConfig.java
+++ src/main/java/nl/appsource/stream/demo/config/CitatenSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebFluxSecurity
 public class CitatenSecurityConfig {
@@ -13,6 +17,14 @@ public class CitatenSecurityConfig {
 
         http.csrf().disable();
         http.authorizeExchange(exchanges -> exchanges.anyExchange().permitAll());
+
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:3000", "https://citaten.odee.net/"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        http.cors().configurationSource(source);
 
         return http.build();
     }
