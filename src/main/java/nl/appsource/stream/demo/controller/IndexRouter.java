5faf8960c41541f917209f6e25f9db128c2b490c
Bas Huisman <bhuism@odee.net>
2020-04-07 22:42:48 +0200

    add x-version header redirect root

--- src/main/java/nl/appsource/stream/demo/controller/IndexRouter.java
+++ src/main/java/nl/appsource/stream/demo/controller/IndexRouter.java
@@ -3,14 +3,13 @@ package nl.appsource.stream.demo.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import static java.net.URI.create;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.ServerResponse.permanentRedirect;
 
 @Slf4j
 @Configuration
@@ -19,8 +18,7 @@ public class IndexRouter {
     @Bean
     public RouterFunction<ServerResponse> index() {
         return RouterFunctions
-                .route(RequestPredicates.GET("/"), (r) ->
-                        ok().contentType(MediaType.TEXT_HTML).render("index"));
+                .route(GET("/"), r -> permanentRedirect(create("https://citaten.odee.net")).build());
     }
 
 }
