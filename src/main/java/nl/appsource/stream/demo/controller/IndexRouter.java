ec5a1c179fb356a1ad463072aadda8c53b6edc60
bhuism <bhuism@odee.net>
2020-10-22 23:03:48 +0200

    add contract

--- src/main/java/nl/appsource/stream/demo/controller/IndexRouter.java
+++ src/main/java/nl/appsource/stream/demo/controller/IndexRouter.java
@@ -1,11 +1,14 @@
 package nl.appsource.stream.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 import static java.net.URI.create;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
@@ -15,10 +18,18 @@ import static org.springframework.web.reactive.function.server.ServerResponse.pe
 @Configuration
 public class IndexRouter {
 
+    @Value("classpath:/static/openapi.yml")
+    private Resource contract;
+
     @Bean
     public RouterFunction<ServerResponse> index() {
         return RouterFunctions
-                .route(GET("/"), r -> permanentRedirect(create("https://citaten.odee.net")).build());
+            .route(GET("/"), r -> permanentRedirect(create("https://citaten.odee.net")).build());
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> contract() {
+        return RouterFunctions.resources((r) -> Mono.just(contract));
     }
 
 }
