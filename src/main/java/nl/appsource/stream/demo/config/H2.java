c02d239ac76491bc26c6a8a4f1d2cd12ed398905
bhuism <bhuism@odee.net>
2020-12-17 16:56:06 +0100

    add h2 console

---
+++ src/main/java/nl/appsource/stream/demo/config/H2.java
@@ -0,0 +1,32 @@
+package nl.appsource.stream.demo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Profile("development")
+public class H2 {
+
+    private Server webServer;
+
+    private final Integer h2ConsolePort = 8081;
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void start(final ContextRefreshedEvent ignore) throws java.sql.SQLException {
+        log.info("Starting h2 console at port " + h2ConsolePort);
+        this.webServer = org.h2.tools.Server.createWebServer("-webPort", h2ConsolePort.toString(), "-tcpAllowOthers").start();
+    }
+
+    @EventListener(ContextClosedEvent.class)
+    public void stop(final ContextRefreshedEvent ignore) {
+        log.info("stopping h2 console at port " + h2ConsolePort);
+        this.webServer.stop();
+    }
+
+}
