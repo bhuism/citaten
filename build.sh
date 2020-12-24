68c97aaf378b5efe34e5d984bcabd01cba4fbc45
bhuism <bhuism@odee.net>
2020-12-24 14:22:25 +0100

    fix env

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ export SHORT_SHA=`git rev-parse --short HEAD`
 export BRANCH_NAME=`git branch --show-current`
 
 mvn -B -Pnative clean spring-boot:build-image && docker push bhuism/citaten-api:latest
+
+cat ./target/classes/git.properties
