3435313f7ab8fb83dae901a7c107c4ec8c6cb833
bhuism <bhuism@odee.net>
2020-12-24 13:58:58 +0100

    fix env

---
+++ buid.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export COMMIT_SHA=`git rev-parse HEAD`
+export SHORT_SHA=`git rev-parse --short HEAD`
+export BRANCH_NAME=`git branch --show-current`
+
+mvn -B -Pnative clean spring-boot:build-image && docker push bhuism/citaten-api:latest
